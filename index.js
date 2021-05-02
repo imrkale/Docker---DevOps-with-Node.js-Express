c02aa1bb3a3347d4ee5cef33a4faeb6126f587ec
Raj Kale <kale44772@gmail.com>
2021-05-02 14:47:44 +0530

    docker image building and running container

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+
+const app = express()
+const port=process.env.PORT || 2000
+
+app.get("/", (req, res) =>{
+    res.send("<h1>Server Running</h1>")
+})
+
+app.listen(port,()=>console.log(`Listening to port: ${port}`))
